75638517ce3fab962289c07a19a01503fcbff1a6
Kate Kumpanska <kate.kumpansky@gmail.com>
2025-05-27 18:18:30 +0300

    add person class, coach class and inheritance

---
+++ fitness_club/CoachClass.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace fitness_club
+{
+    class CoachClass : Person
+    {
+        private string? fitnessServices;
+        public string? FitnessServices
+        {
+            get { return fitnessServices; }
+            set { fitnessServices = value; }
+        }
+
+    }
+}
---
+++ fitness_club/Person.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace fitness_club
+{
+    class Person
+    {
+        private int id;
+        private string? name;
+        private string? lastName;
+        private string? phoneNumber;
+        private string? email;
+        public int Id
+        {
+            get { return id; }
+            set {  id = value; }
+        }
+        public string? Name 
+        {
+            get { return name; }
+            set { name = value; }
+        }
+        public string? LastName
+        {
+            get { return lastName; }
+            set { lastName = value; }
+        }
+        public string? PhoneNumber
+        {
+            get { return phoneNumber; }
+            set { phoneNumber = value; }
+        }
+        public string? Email
+        {
+            get { return email; }
+            set { email = value; }
+        }
+    }
+}
