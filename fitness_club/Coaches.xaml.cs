b674d1544992dec656ffc0bd879490a0fdb3c9db
Kate Kumpanska <kate.kumpansky@gmail.com>
2025-05-28 11:14:23 +0300

    add code-behind coaches form and code-behind edit client information form,add changes in forms

---
+++ fitness_club/Coaches.xaml.cs
@@ -0,0 +1,94 @@
+﻿using fitness_club.Classes;
+using Microsoft.Data.SqlClient;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace fitness_club
+{
+    /// <summary>
+    /// Interaction logic for Coaches.xaml
+    /// </summary>
+    public partial class Coaches : Window
+    {
+        private ObservableCollection<CoachClass> coaches = new ObservableCollection<CoachClass>();
+        private const string connectionString = "Server=DESKTOP-K1I43VD;Database=master;TrustServerCertificate=True;Trusted_Connection=True";
+        public Coaches()
+        {
+            InitializeComponent();
+            LoadCoaches();
+        }
+        private void LoadCoaches()
+        {
+            coaches.Clear();
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+                string query = "SELECT [Id], [Last Name], [Name],[Middle Name],[Phone Number],[Email], [Fitness Services] FROM [Table_Coaches]";
+                SqlCommand cmd = new SqlCommand(query, connection);
+                SqlDataReader reader = cmd.ExecuteReader();
+                while (reader.Read())
+                {
+                    coaches.Add(new CoachClass
+                    {
+                        Id = (int)reader["Id"],
+                        LastName = reader["Last Name"].ToString(),
+                        Name = reader["Name"].ToString(),
+                        MiddleName = reader["Middle Name"].ToString(),
+                        PhoneNumber = reader["Phone Number"].ToString(),
+                        Email = reader["Email"].ToString(),
+                        FitnessServices  = reader["Fitness Services"].ToString(),
+                    });
+                }
+                CoachesListView.ItemsSource = coaches;
+            }
+        }
+        private void EditCoach_Click(object sender, RoutedEventArgs e)
+        {
+            if (CoachesListView.SelectedItem is CoachClass selected)
+            {
+                var window = new EditCoachInformationForm(selected);
+                if (window.ShowDialog() == true)
+                {
+                    LoadCoaches();
+                }
+            }
+        }
+        private void DeleteCoach_Click(object sender, RoutedEventArgs e)
+        {
+            if (CoachesListView.SelectedItem is CoachClass selected)
+            {
+                var result = MessageBox.Show($"Ви впевнені, що хочете видалити тренера: {selected.LastName} {selected.Name} {selected.MiddleName}?", "Підтвердження", MessageBoxButton.YesNo);
+                if (result == MessageBoxResult.Yes)
+                {
+                    using (SqlConnection connection = new SqlConnection(connectionString))
+                    {
+                        connection.Open();
+                        string query = "DELETE FROM [Table_Coaches] WHERE Id = @Id";
+                        SqlCommand cmd = new SqlCommand(query, connection);
+                        cmd.Parameters.AddWithValue("@Id", selected.Id);
+                        cmd.ExecuteNonQuery();
+                    }
+                    LoadCoaches();
+                }
+            }
+        }
+        private void CoachesListView_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            bool isItemSelected = CoachesListView.SelectedItem is CoachClass;
+            EditButton.IsEnabled = isItemSelected;
+            DeleteButton.IsEnabled = isItemSelected;
+        }
+    }
+}
---
+++ fitness_club/EditClientInformationForm.xaml.cs
@@ -0,0 +1,85 @@
+﻿using fitness_club.Classes;
+using Microsoft.Data.SqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace fitness_club
+{
+    /// <summary>
+    /// Interaction logic for EditClientInformationForm.xaml
+    /// </summary>
+    public partial class EditClientInformationForm : Window
+    {
+        private readonly int clientId;
+        private const string connectionString = "Server=DESKTOP-K1I43VD;Database=master;TrustServerCertificate=True;Trusted_Connection=True";
+        public EditClientInformationForm(ClientClass client)
+        {
+            InitializeComponent();
+            clientId = client.Id;
+            ClientNameText.Text = client.Name;
+            ClientLastNameText.Text = client.LastName;
+            ClientMiddleNameText.Text = client.MiddleName;
+            ClientPhoneNumberText.Text = client.PhoneNumber;
+            ClientEmailText.Text = client.Email;
+        }
+        private void SaveButton_Click(object sender, RoutedEventArgs e)
+        {
+            string name = ClientNameText.Text.Trim();
+            string lastName = ClientLastNameText.Text.Trim();
+            string middleName = ClientMiddleNameText.Text.Trim();
+            string phone = ClientPhoneNumberText.Text.Trim();
+            string email = ClientEmailText.Text.Trim();
+            if (string.IsNullOrWhiteSpace(name) || string.IsNullOrWhiteSpace(lastName) ||
+       string.IsNullOrWhiteSpace(phone) || string.IsNullOrWhiteSpace(email))
+            {
+                MessageBox.Show("Будь ласка, заповніть усі поля.", "Помилка", MessageBoxButton.OK, MessageBoxImage.Warning);
+                return;
+            }
+            try
+            {
+                using (SqlConnection connection = new SqlConnection(connectionString))
+                {
+                    connection.Open();
+                    string query = @"UPDATE [Table_Clients]
+                             SET [Name] = @Name,
+                                 [Last Name] = @LastName,
+                                 [Middle Name] = @MiddleName,
+                                 [Phone Number] = @PhoneNumber,
+                                 [Email] = @Email
+                             WHERE [Id] = @Id";
+                    SqlCommand cmd = new SqlCommand(query, connection);
+                    cmd.Parameters.AddWithValue("@Name", name);
+                    cmd.Parameters.AddWithValue("@LastName", lastName);
+                    cmd.Parameters.AddWithValue("@MiddleName", middleName);
+                    cmd.Parameters.AddWithValue("@PhoneNumber", phone);
+                    cmd.Parameters.AddWithValue("@Email", email);
+                    cmd.Parameters.AddWithValue("@Id", clientId);
+                    cmd.ExecuteNonQuery();
+                }
+                MessageBox.Show("Інформацію про тренера оновлено!", "Успіх", MessageBoxButton.OK, MessageBoxImage.Information);
+                DialogResult = true;
+                Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Помилка збереженні: " + ex.Message, "Помилка", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+        private void CancelButton_Click(object sender, RoutedEventArgs e)
+        {
+            DialogResult = false;
+            Close();
+        }
+    }
+}
