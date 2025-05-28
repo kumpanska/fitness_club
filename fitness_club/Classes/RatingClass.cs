5763682f50815bf3f0358175e9a48118edf05a16
Kate Kumpanska <kate.kumpansky@gmail.com>
2025-05-28 19:23:56 +0300

    add change in schedule class, add rating class

---
+++ fitness_club/Classes/RatingClass.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace fitness_club.Classes
+{
+    internal class RatingClass
+    {
+        private int id;
+        private int coachId;
+        private double averageMark;
+        public int Id
+        {
+            get { return id; }
+            set { id = value; }
+        }
+        public int CoachId
+        {
+            get { return coachId; }
+            set { coachId = value; }
+        }
+        public double AverageMark
+        {
+            get { return averageMark; }
+            set { averageMark = value; }
+        }
+
+    }
+}
