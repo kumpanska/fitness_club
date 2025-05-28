51bce6cbe9fc7447742dc0ef5498956d794631be
Kate Kumpanska <kate.kumpansky@gmail.com>
2025-05-28 10:27:46 +0300

    renew folder for classes

---
+++ fitness_club/Classes/ExerciseClass.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace fitness_club
+{
+    public class ExerciseClass
+    {
+        private int id;
+        private string? name;
+        private string? type;
+        private int repetitions;
+        public int Id
+        {
+            get { return id; }
+            set { id = value; }
+        }
+        public string? NameOfExercise
+        {
+            get { return name; }
+            set { name = value; }
+        }
+        public string? Type
+        {
+            get { return type; }
+            set { type = value; }
+        }
+        public int Repetitions
+        {
+            get { return repetitions; }
+            set { repetitions = value; }
+        }
+    }
+}
+
---
+++ fitness_club/Classes/LoadExerciseTypesClass.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.Data.SqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows;
+
+namespace fitness_club
+{
+    public static class LoadExerciseTypesClass
+    {
+        private const string connectionString = "Server=DESKTOP-K1I43VD;Database=master;TrustServerCertificate=True;Trusted_Connection=True";
+        public static void LoadExerciseTypesIntoComboBox(ComboBox comboBox)
+        {
+            comboBox.Items.Clear();
+            try
+            {
+                using (SqlConnection connection = new SqlConnection(connectionString))
+                {
+                    connection.Open();
+                    string query = "SELECT DISTINCT [Type] FROM [Table_Exercises] WHERE [Type] IS NOT NULL AND [Type] <> ''";
+
+                    using (SqlCommand cmd = new SqlCommand(query, connection))
+                    using (SqlDataReader reader = cmd.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            string? type = reader["Type"]?.ToString();
+                            if (!string.IsNullOrWhiteSpace(type))
+                            {
+                                comboBox.Items.Add(new ComboBoxItem { Content = type });
+                            }
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Помилка при завантаженні типів вправ: " + ex.Message, "Помилка", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+    }
+}
